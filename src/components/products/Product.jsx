47c493eebcce1df2d101db2c5ad1d8d648b9359d
simicodes <simeonoffiong@gmail.com>
2022-05-31 10:43:14 -0600

    first commit

---
+++ src/components/products/Product.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import useStyles from "./styles";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+const Product = ({ product }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={product.image} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="body2" color="textSecondary">
+          {product.description}
+        </Typography>
+        <CardActions disableSpacing className={classes.cardActions}>
+          <IconButton aria-label="Add to Cart">
+            <AddShoppingCart />
+          </IconButton>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Product;
